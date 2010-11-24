bdcf91c53725b85dbbedaf2c7b0f17eb8c79d188
Christoph Helma <helma@in-silico.ch>
2010-11-24 12:59:19 +0100

    initial import

---
+++ ot-config.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'rush'
+
+DEVELOPMENT = Rush::Box.new('localhost')
+
+DEVELOPMENT_DIRS = {
+	:compound => DEVELOPMENT["/var/www/opentox/compound/"],
+	:dataset => DEVELOPMENT["/var/www/opentox/dataset/"],
+	:algorithm => DEVELOPMENT["/var/www/opentox/algorithm/"],
+	:model => DEVELOPMENT["/var/www/opentox/model/"],
+	:task => DEVELOPMENT["/var/www/opentox/task/"],
+	:toxcreate => DEVELOPMENT["/var/www/opentox/toxcreate/"],
+	:gem => DEVELOPMENT["/var/www/opentox/opentox-ruby-api-wrapper/"]
+}
